3cb071f73dbf93f65d2c60ed4e9a06bd60cebf11
JingleJangle1998 <31698765+JingleJangle1998@users.noreply.github.com>
2018-01-17 11:57:46 +0100

    Update and rename dffs.py to CreateTable.py

--- CreateTable.py
+++ CreateTable.py
@@ -21,14 +21,6 @@ telefoonnummer VARCHAR(255),
 IBAN VARCHAR(255));"""
 
 cursor.execute(sql_command)
-
-sql_command = """INSERT INTO klantgegevens (geslacht, voornaam, achternaam, geboortedatum, postcode, huisnummer, straatnaam, woonplaats, email, telefoonnummer, iban)
-    VALUES (NULL, 'man', 'sohaib', 'elb', '1961-10-25', '3515GN', 51, 'lauwerecht', 'utrecht', 's_elb@outlook.com', '0619096355', 'NL79281894');"""
-cursor.execute(sql_command)
-
-
-
 # never forget this, if you want the changes to be saved:
 connection.commit()
-
 connection.close()
