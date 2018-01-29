09a73152a6a9629bb737619cc1ee74ee5913181f
JingleJangle1998 <31698765+JingleJangle1998@users.noreply.github.com>
2018-01-29 13:36:04 +0100

    Add files via upload

--- Invoegenfitnessapparaten.py
+++ Invoegenfitnessapparaten.py
@@ -1,44 +1,33 @@
-import sqlite3
 import pymysql
 import pymysql.cursors
-connection = pymysql.connect(host='localhost',
-                             user='root',
-                             password='schaap14',
-                             db='company',
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
-c = connection.cursor()
+conn = pymysql.connect(host='188.166.116.67',
+                               user='groep5',
+                               password='HWu4RTsD8&@UUN',
+                               db='groep5_benno',
+                               charset='utf8mb4',
+                               cursorclass=pymysql.cursors.DictCursor)
+c = conn.cursor()
 
 
 c.execute("""INSERT INTO apparaten (apparaatID, naam, kcalperminuut)
-
     VALUES (NULL, 'hardlopen', 45
-
 );""")
 
 c.execute("""INSERT INTO apparaten (apparaatID, naam, kcalperminuut)
-
     VALUES (NULL, 'fietsen', 60
-
 );""")
 
 c.execute("""INSERT INTO apparaten (apparaatID, naam,kcalperminuut)
-
     VALUES (NULL, 'benchpress' ,2
-
 );""")
 
 c.execute("""INSERT INTO apparaten (apparaatID, naam, kcalperminuut)
-
     VALUES (NULL, 'roeien', 84
-
 );""")
 
 c.execute("""INSERT INTO apparaten (apparaatID, naam, kcalperminuut)
-
     VALUES (NULL, 'pulldown', 1 
-
 );""")
 # never forget this, if you want the changes to be saved:
-connection.commit()
-connection.close()
+conn.commit()
+conn.close()
